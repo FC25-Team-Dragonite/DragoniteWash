64395d20e1baa4468294d92f2ee54e681e33008b
Tyler <tylerpolite@Tylers-MacBook-Air-2.local>
2025-03-12 19:23:00 -0400

    Washers and Divs added to Landing Page

--- client/src/Washer.jsx
+++ client/src/Washer.jsx
@@ -1,1 +1,14 @@
 import React, { Component } from 'react';
+import Washer from './assets/Washer.png';
+
+class WasherIcon extends Component {
+  render() {
+    return (
+      <div>
+        <img src={Washer} alt='Washer Icon' />
+      </div>
+    );
+  }
+}
+
+export default WasherIcon;
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,7 +1,25 @@
+import Washer from '../../src/Washer';
 const LandingPage = () => {
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <svg class='animate-bounce w-6 h-6 ...'>
+        <div>
+          <Washer />
+        </div>
+      </svg>
+      <div>
+        <Washer />
+      </div>{' '}
+      <div>
+        <Washer />
+      </div>{' '}
+      <div>
+        <Washer />
+      </div>{' '}
+      <div>
+        <Washer />
+      </div>{' '}
     </div>
   );
 };
