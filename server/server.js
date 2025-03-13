a34fc69598c6501f302ae1c84a050e4343805c91
Salisu Ibrahim <salisu.ibrahim15@yahoo.com>
2025-03-13 12:18:47 -0400

    working on the api

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import laundryMatsRouter from '../server/testApi';
+import pg from 'pg'
+
+// Load environment variables
+dotenv.config();
+
+// Initialize Express app
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+// Middleware to parse incoming URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+
+// Use laundry mat routes
+app.use('/api/laundry-mats', laundryMatsRouter);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
