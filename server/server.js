badca8ae74a26971bcaf4fd3f07e004c659767ac
Salisu Ibrahim <salisu.ibrahim15@yahoo.com>
2025-03-13 17:31:12 -0400

    merged from dev

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import laundryMatsRouter from './laundryMats.js';
-import pg from 'pg'
+import pg from 'pg';
+import path from 'path';
 
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 // Load environment variables
 dotenv.config();
 
@@ -17,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Use laundry mat routes
 app.use('/machines', laundryMatsRouter);
-
+app.use(express.static(path.join(__dirname, "../client" )))
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
